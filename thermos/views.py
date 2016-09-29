22bbe8c6f83ea49aeb1b92cf49f4bc159ae8f936
Eugene Viktorov <jekafromua@gmail.com>
2016-09-30 00:45:25 +0300

    Correct imports

--- thermos/views.py
+++ thermos/views.py
@@ -1,17 +1,17 @@
 from flask import render_template, flash, redirect, url_for, abort
 
 from thermos import app, db
-from forms import BookmarkForm
-from models import User, Bookmark
+from .forms import BookmarkForm
+from .models import User, Bookmark
 
 #Fake login
 def logged_in_user():
-	return models.User.query.filter_by(username='reindert').first()
+	return User.query.filter_by(username='reindert').first()
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', new_bookmarks = models.Bookmark.newest(5))
+	return render_template('index.html', new_bookmarks = Bookmark.newest(5))
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -20,7 +20,7 @@ def add():
 	if form.validate_on_submit():
 		url = form.url.data
 		description = form.description.data
-		bm = models.Bookmark(user=logged_in_user(), url=url, description=description)
+		bm = Bookmark(user=logged_in_user(), url=url, description=description)
 		db.session.add(bm)
 		db.session.commit()
 		flash("Stored: '{}'".format(description))
